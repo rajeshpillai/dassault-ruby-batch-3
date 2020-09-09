f7af79f2dde11a949ee2745ac67a172fe3d55246
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-09-09 17:40:37 +0530

    Moved modules to modules folder

--- 19-module-include-demo.rb
+++ 19-module-include-demo.rb
@@ -1,25 +1,4 @@
-require "digest"  # encry/decryp
-
-module Encryption
-  def encrypt(str) 
-    Digest::SHA2.hexdigest(str)
-  end
-
-  def decrypt(estr) 
-  
-  end
-end
-
-module HackyEncryption
-  def encrypt(str) 
-    "you are hacked"
-  end
-
-  def decrypt(estr) 
-  
-  end
-end
-
+require_relative './modules/digest'
 
 class Base 
 end
---
+++ modules/digest.rb
@@ -0,0 +1,21 @@
+require "digest"  # encry/decryp
+
+module Encryption
+  def encrypt(str) 
+    Digest::SHA2.hexdigest(str)
+  end
+
+  def decrypt(estr) 
+  
+  end
+end
+
+module HackyEncryption
+  def encrypt(str) 
+    "you are hacked"
+  end
+
+  def decrypt(estr) 
+  
+  end
+end
