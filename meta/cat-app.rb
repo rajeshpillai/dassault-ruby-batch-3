7d4f4256c1d8eaefcf350ea4f27e800cf0f25ceb
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-09-10 15:06:08 +0530

    method_missing

--- meta/cat-app.rb
+++ meta/cat-app.rb
@@ -1,12 +1,13 @@
 # Opening an existing object class
 
+# Opening the Object class
 class Object 
   # Overriding the class method const_missing method of the Object
-  def self.const_missing(c) 
-    puts "#{c} is missing.  So, let's load it dynamically."
-    require_relative "./#{c.downcase}"
+  def self.const_missing(const_name) 
+    puts "#{const_name} is missing.  So, let's load it dynamically."
+    require_relative "./#{const_name.downcase}"
     # Cat
-    klass = const_get(c)
+    klass = const_get(const_name)
     return klass  if klass 
   end
 end
---
+++ meta/method_missing.rb
@@ -0,0 +1,16 @@
+class Numeric 
+  @@currencies = { yen: 1.55, euro: 0.013, usd: 0.014}
+  def method_missing(method_name, *args, &block)
+    p self
+    puts "#{method_name} not found"
+    
+    if @@currencies.has_key?(method_name)
+      self / @@currencies[method_name]
+    else 
+      super 
+    end
+  end
+end
+
+puts 100.usd
+puts 200.euro
