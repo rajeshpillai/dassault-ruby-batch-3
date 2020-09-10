38646cea58c0e7430dc82c277776e6089814a3d1
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-09-10 14:47:34 +0530

    const_get

--- meta/cat-app.rb
+++ meta/cat-app.rb
@@ -4,12 +4,14 @@ class Object
   # Overriding the class method const_missing method of the Object
   def self.const_missing(c) 
     puts "#{c} is missing.  So, let's load it dynamically."
-    require_relative "./cat"
-    Cat
+    require_relative "./#{c.downcase}"
+    # Cat
+    klass = const_get(c)
+    return klass  if klass 
   end
 end
 
 puffy_cat = Cat.new
 puffy_cat.talk
 
-Dog.new
+
